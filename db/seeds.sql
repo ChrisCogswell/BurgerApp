06eee0ff9b658878906e6ba391b1c4698d0786ba
ChrisCogs <ccogs@yahoo.com>
2019-05-26 15:36:49 -0400

    Initial commit with npm packages installed and mysql database set up

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Double Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Southwest burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie burger');
